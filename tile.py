205ca51b418b22797f59c82eb1f7ed1bb4806183
ullesdiykanal <privat@sascha-ulrich.de>
2022-02-01 17:25:56 +0100

    Update tile.py

--- tile.py
+++ tile.py
@@ -4,5 +4,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
       super().__init__()
-      self.image = pygame.image.load('../assets/test/rock.png')
+      self.image = pygame.image.load('../assets/test/rock.png').convert_alpha()
       self.rect = self.image.get_rect(topleft = pos)
