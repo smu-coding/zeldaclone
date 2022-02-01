e7e08139764cee30a3217c00b3d644937c690cc6
ullesdiykanal <privat@sascha-ulrich.de>
2022-02-01 18:05:47 +0100

    Update level.py

--- level.py
+++ level.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -9,7 +12,20 @@ class Level:
         ### SPRITE GROUP SETUP ###
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        ### SPRITE SETUO ###
+        self.create_map()
     
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y),[self.visible_sprites, self.obstacles_sprites])
+                if col == 'p':
+                    Player((x, y),[self.visible_sprites])
+
     ### UPDATE AND DRAW THE LEVEL ###
     def run(self):
-        pass
+        self.visible_sprites.draw(self.display_surface)
