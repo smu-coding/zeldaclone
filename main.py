744e61e54a9d582118261e7b58a2ab8e777f9447
ullesdiykanal <privat@sascha-ulrich.de>
2022-02-01 17:14:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from settings import *
 from debug import debug
+from level import Level
  
 class Game:
     def __init__(self):
@@ -9,7 +10,8 @@ class Game:
         pygame.display.set_caption('Zelda Clone 1.0a')
 
         self.clock = pygame.time.Clock()
-    
+        self.level = Level()
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -18,6 +20,7 @@ class Game:
                     sys.exit()
  
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
  
