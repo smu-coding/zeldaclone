88ecd389638ab1c4156253407a4fcb546b1367e8
ullesdiykanal <privat@sascha-ulrich.de>
2022-02-03 00:13:46 +0100

    update player.py

--- player.py
+++ player.py
@@ -4,7 +4,7 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
       super().__init__(groups)
-      self.image = pygame.image.load('zeldaclone/assets/player.png').convert_alpha()
+      self.image = pygame.image.load('assets/player.png').convert_alpha()
       self.rect = self.image.get_rect(topleft = pos)
 
       self.direction = pygame.math.Vector2()
